5e09a0d796f600c18c20835af9c7c9e3acd26b23
AlubuArt <jc@jcvisueldesign.dk>
2021-03-10 15:17:18 +0100

    component varelinje

--- src/sections/afhentninger/opretafhentning.js
+++ src/sections/afhentninger/opretafhentning.js
@@ -1,8 +1,86 @@
+import {useEffect, useState} from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { CardActionArea, CardHeader, Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import './opretafhentning.css'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import EditIcon from '@material-ui/icons/Edit';
+import VareLinje from './vareLinje';
+
+
 const OpretAfhentning = () => {
+
+    const [afhentning, setAfhentning] = useState()
+    const [varer, setVarer] = useState([{
+                                title: "Salat",
+                                mængde: "50",
+                                mængdeEnhed: "kg",
+                                tidsrum: "08.00-16.00"
+                            },
+                            {
+                                title: "Salat",
+                                mængde: "50",
+                                mængdeEnhed: "kg",
+                                tidsrum: "08.00-16.00"
+                            },{
+                                title: "Salat",
+                                mængde: "50",
+                                mængdeEnhed: "kg",
+                                tidsrum: "08.00-16.00"
+                        }])
+
+
+   
+
+    useEffect(() => {
+
+    })
+
+
+    
 
     return (
         <div>
-            <h1>Opretafhentning</h1>
+            <Card className="opret-afhentning-card">
+                <CardActionArea>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5">
+                           Aktuel afhentning
+                        </Typography> 
+                        
+                            <Grid container>
+                                <Grid item xs={4} component="div">
+                                    <p>Type</p>
+                                </Grid>
+                                <Grid item xs={2} component="div">
+                                    <p>Mængde</p>
+                                </Grid>
+                                <Grid item xs={4} component="div">
+                                    <p>Tidsrum</p>
+                                </Grid>
+                                
+                            </Grid>
+                        
+                        {
+                            varer.map((vare, i) => {
+                                return <VareLinje 
+                                            title={vare.title}
+                                            mængde={vare.mængde}
+                                            mængdeEnhed={vare.mængdeEnhed}
+                                            tidsrum={vare.tidsrum}
+                                            key={i}
+                                        />
+                            })
+                        }
+                        
+                    </CardContent>  
+                </CardActionArea>
+                
+            </Card>
         </div>
     )
 }
---
+++ src/sections/afhentninger/vareLinje.js
@@ -0,0 +1,44 @@
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { CardActionArea, Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import './opretafhentning.css'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import EditIcon from '@material-ui/icons/Edit';
+
+const VareLinje = (props) => {
+
+
+
+    return (
+
+        <Card spacing={2}xs="12">
+                             <Grid container >
+                                <Grid item xs={4} component="div">
+                                       <p>{props.title}</p>
+                                </Grid>
+                                <Grid item xs={2} component="div">
+                                        <p>{props.mængde}{props.mængdeEnhed}</p>
+                                </Grid>
+                                <Grid item xs={4}>
+                                    <p>kl.{props.tidsrum}</p>
+                                </Grid>
+                                <Grid item xs={1} component="div">
+                                  <DeleteOutlinedIcon />
+                                </Grid>
+                                <Grid item xs={1} component="div">
+                                   <EditIcon />
+                                </Grid>
+
+                             </Grid>
+
+                        </Card> 
+    )
+}
+
+export default VareLinje;
+
+ 
