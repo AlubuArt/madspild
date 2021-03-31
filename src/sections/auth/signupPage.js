f274c7f311a1810764678dfcafc8ad9ff34f4118
AlubuArt <jc@jcvisueldesign.dk>
2021-03-31 08:29:08 +0200

    signup layout done

--- src/sections/auth/signupPage.js
+++ src/sections/auth/signupPage.js
@@ -1,18 +1,68 @@
-import {useState} from 'react'
+import {useState, useReducer} from 'react'
 import {signupUserInDatabase} from '../../service/login.service'
 import {firebase_app } from '../../service/firebase.config'
+import {Container} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import CardHeader from '@material-ui/core/CardHeader';
+import { useContainedCardHeaderStyles } from '@mui-treasury/styles/cardHeader/contained';
+import { useSoftRiseShadowStyles } from '@mui-treasury/styles/shadow/softRise';
+import { useFadedShadowStyles } from '@mui-treasury/styles/shadow/faded';
+import Card from '@material-ui/core/Card';
+import cx from 'clsx';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import { CardActionArea } from '@material-ui/core';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles(({ spacing }) => ({
+    card: {
+        marginTop: 40,
+        borderRadius: spacing(0.5),
+        transition: '0.3s',
+        width: '95%',
+        //overflow: 'initial',
+        background: '#ffffff',
+      },
+      content: {
+        paddingTop: 0,
+        textAlign: 'left',
+        overflowX: 'auto',
+        '& table': {
+          marginBottom: 0,
+        }
+      },
+
+  }));
 
 
 
 const SignupPage = ({value, onChange}) => {
 
-    const [email, setEmail] = useState();
+    
     const [pass, setPass] = useState();
+    const classes = useStyles();
+    const cardHeaderStyles = useContainedCardHeaderStyles();
+    const cardShadowStyles = useSoftRiseShadowStyles({ inactive: true });
+    const cardHeaderShadowStyles = useFadedShadowStyles();
+    const [profileData, setProfileData] = useReducer((value, newValue) => ({...value, ...newValue}), {
+        virksomhedsNavn: '',
+        virksomhedsCVR: '',
+        kontaktEmail: '',
+
+    })
+
+    const handleSignupClick =(e) => {
+        e.preventDefault();
+        signup();
+    }
 
     const signup = async () => {
+        onChange(0)
         try {
-          await signupUserInDatabase(email, pass); 
+          await signupUserInDatabase(profileData.kontaktEmail, pass); 
         } catch (error) {
             console.log(error)
         }
@@ -22,7 +72,7 @@ const SignupPage = ({value, onChange}) => {
 
     const redirectToHomePageAfterSuccess = async () => {
         try{
-            await firebase_app.auth().signInWithEmailAndPassword(email, pass);
+            await firebase_app.auth().signInWithEmailAndPassword(profileData.kontaktEmail, pass);
             onChange(0)
 
         } catch (error) {
@@ -32,9 +82,45 @@ const SignupPage = ({value, onChange}) => {
 
 
     return (
-        <>
-        Signup!
-        </>
+        <Container fluid='true'>
+            <CardHeader
+                className={cardHeaderShadowStyles.root}
+                classes={cardHeaderStyles}
+                title={'Opret ny profil'}
+                subheader={'For at kunne registrere overskudsmad, så det kan blive afhentet, skal du registrere din virksomhed'}
+                />
+            <Card className={cx(classes.card, cardShadowStyles.root)}>
+                <CardActionArea>
+                    <CardContent className={classes.content}>
+                        <TextField  
+                            className="form-control" 
+                            type="text" 
+                            label="Virksomhedens navn"
+                            onChange={(e) => setProfileData({virksomhedsNavn: e.target.value})}/>
+                        <TextField 
+                            className="form-control" 
+                            type="text" 
+                            label="Virksomhedens cvrnr."
+                            onChange={(e) => setProfileData({virksomhedsCVR: e.target.value})}/>
+                        <TextField 
+                            className="form-control" 
+                            type="text" 
+                            label="Kontaktperson email"
+                            onChange={(e) => setProfileData({kontaktEmail: e.target.value})}/>
+                        <TextField 
+                            className="form-control" 
+                            type="password" 
+                            placeholder="*****" 
+                            label="Adgangskode"
+                            onChange={(e) => setPass(e.target.value)}
+                        />
+                    </CardContent>
+                    <Button style={{margin: '10px'}} variant="contained" onClick={handleSignupClick}>Opret bruger</Button>
+                </CardActionArea>
+            </Card>
+            
+
+        </Container>
 
     )
 }
